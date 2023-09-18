993efdcce9392e737ad9004b762851cb0fb0d282
amitke841 <57447208+amitke841@users.noreply.github.com>
2023-09-18 11:40:32 +0000

    .

--- main.java
+++ main.java
@@ -1,7 +1,14 @@
-public class main {
-
+import java.util.*;
+public class main
+{
 	public static void main(String args[]){
-		var Name = "Amit";
-		System.out.println("My name is " + Name);
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter your age:");
+		int age = sc.nextInt();
+		System.out.println("Your age is "+age);
+		System.out.println("How much years you want to calculate?");
+		int years = sc.nextInt();
+		int ageFuture = age+years;
+		System.out.println("Your age in "+years+" years will be "+ageFuture);
 	}
 }
