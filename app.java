a03982bc305eaf265e7692173b5a017c8b761489
amitke841 <57447208+amitke841@users.noreply.github.com>
2023-10-30 12:07:40 +0000

    .

--- app.java
+++ app.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Your Number is: " + num);
     }
 }
