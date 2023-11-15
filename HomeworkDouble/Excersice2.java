2847804d6bbd18424b5bed44befe9e36812eaa96
amitke841 <amiti.keshet@gmail.com>
2023-11-15 16:27:18 +0200

    .

--- HomeworkDouble/Excersice2.java
+++ HomeworkDouble/Excersice2.java
@@ -5,11 +5,17 @@ public class Excersice2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Weight of Rectangle: ");
-        double weight = sc.nextDouble();
+        double length = sc.nextDouble();
         System.out.print("Enter Height of Rectangle: ");
         double height = sc.nextDouble();
-        System.out.println("The current area of the rectangle is: " + weight * height);
-
-        
+        double rec1 = length * height;
+        System.out.println("The current area of the rectangle is: " + rec1);
+        length = length *2;
+        height = height -1;
+        double rec2 = length * height;
+        System.out.println("The new area of the rectangle is: " + rec2);
+        double recSum = rec2 - rec1;
+        System.out.println("The difference between the two areas is: " + recSum);
+        sc.close();
     }
 }
