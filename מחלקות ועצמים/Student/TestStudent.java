861651649e7bd2378656249f63d5d59eddd79462
amitke <amiti.keshet@gmail.com>
2025-04-27 12:15:52 +0300

    .

--- מחלקות ועצמים/Student/TestStudent.java
+++ מחלקות ועצמים/Student/TestStudent.java
@@ -1,6 +1,12 @@
 package Student;
 
 public class TestStudent {
+    //ט.כניסה: הפעולה מקבלת סטודנט
+    //ט.יציאה: הפעולה מחזירה את ממוצע הציונים שלו
+    public static double avgStudent(Student stu) {
+        return (stu.getMath() + stu.getEnglish() + stu.getLanguage()) / 3.0;
+    }
+
     public static void main(String[] args) {
         Student student1 = new Student("Alice", "123456789", 90, 85, 88);
         Student student2 = new Student();
@@ -17,6 +23,56 @@ public class TestStudent {
             System.out.println("Student with the highest average: " + student2.getName());
         } else {
             System.out.println("Both students have the same average");
+        }
+
+        Student[] arr = new Student[4];
+
+        //קולטת סטודנטים למערך
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Student();
+        }
+
+        //ממוצע ציונים של כל תלמיד
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Student " + i + " avg is: " + avgStudent(arr[i]));
+        }
+
+        // ממוצע גבוה ביותר
+        Student s;
+        double highestAvg = avgStudent(arr[0]);
+        Student MaxStu = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            s = arr[i];
+            if(avgStudent(s) > highestAvg) {
+                highestAvg = avgStudent(s);
+                MaxStu = s;
+            }
+        }
+        System.out.println("Student with highest Avg: " + MaxStu.getName());
+
+        //ציון ממוצע באנגלית
+        double avgEng = 0;
+        for (int i = 0; i < arr.length; i++) {
+            s = arr[i];
+            avgEng += s.getEnglish();
+        }
+        avgEng = avgEng/arr.length;
+        System.out.println("Avg english: " + avgEng);
+
+        // תלמידים באנגלית שקיבלו מעל הממוצע
+        int countAboveAvgEng = 0;
+        for (int i = 0; i < arr.length; i++) {
+            s = arr[i];
+            if (s.getEnglish() > avgEng) {
+                countAboveAvgEng++;
+            }
+        }
+        System.out.println("num of english got above AVG: " + countAboveAvgEng);
+
+        // בונוס של 10 אחוז בלשון
+        for (int i = 0; i < arr.length; i++) {
+            s = arr[i];
+            s.giveBonus("english", 10);
         }
     }
 }
