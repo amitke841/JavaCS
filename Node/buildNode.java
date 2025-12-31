c4420d5937f98497fd2eea05b28abaf0fc2cef8a
amitke <amiti.keshet@gmail.com>
2025-12-31 11:57:30 +0200

    .

--- Node/buildNode.java
+++ Node/buildNode.java
@@ -3,7 +3,7 @@ public class buildNode {
     	public static Node<Integer> buildUniqueList(Node<Integer> n) {
 		Node<Integer> newN = null,last = null, current;
 		while(n!=null) {
-			if(isAppear(last,n.getValue())) {
+			if(testNode.isAppear(last,n.getValue())) {
 				if (newN == null) {
 					newN = new Node<Integer>(n.getValue());
 					last = newN;
@@ -21,7 +21,7 @@ public class buildNode {
     public static Node<Integer> buildFollowingNums(int beginner, int num) {
         Node<Integer> newN = null,last = null, current;
         int nextNum = beginner;
-        while(nextNum <= num) {
+        while(nextNum <= beginner+num) {
 				if (newN == null) {
 					newN = new Node<Integer>(beginner);
 					last = newN;
@@ -35,4 +35,32 @@ public class buildNode {
 		}
 		return newN;
     }
+
+	public static Node<Integer> buildReverseNode(Node<Integer> n) {
+		Node<Integer> newN = null,last = null, current;
+		int i = testNode.length(n);
+		Node<Integer> nSave = n;
+		while (i > 0) {
+			n = nSave;
+			for (int j = i; j > 1; j--) {
+				n = n.getNext();
+			}
+			if (newN == null) {
+			newN = new Node<Integer>(n.getValue());
+			last = newN;
+            i--;
+			} else {
+				current = new Node<Integer>(n.getValue());
+				last.setNext(current);
+				last = current;
+            	i--;
+			}
+		}
+		return newN;
+	}
+
+	public static void main(String[] args) {
+		Node<Integer> list = new Node<Integer>(1,new Node<Integer>(2,new Node<Integer>(3,new Node<Integer>(4))));
+		System.out.println(buildReverseNode(list));
+	}
 }
